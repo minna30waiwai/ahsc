0ad63e7962851fd16f2eee2252284185d4019961
masafuka_labo_pc <masahirofukada@hiroshima-u.ac.jp>
2018-10-04 17:12:14 +0900

    アクティブオブジェクト名の一覧出力

---
+++ find_active_objects_in_list.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class find_active_objects_in_list : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		//typeで指定した型の全てのオブジェクトを配列で取得し，その要素数繰り返す
+		foreach (GameObject obj in UnityEngine.Object.FindObjectsOfType(typeof(GameObject)))
+		{
+			//シーン上に存在するオブジェクトならば処理
+			if(obj.activeInHierarchy){
+				//GameObjectの名前を表示
+				Debug.Log(obj.name);
+			}
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
